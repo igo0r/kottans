eecbd38bf6400a9d4cd1fbd835eea0f3747d8c9e
igo0r <igor.lantushenko@gmail.com>
2022-08-28 15:20:37 +0300

    Update and rename disable-tracking.sh to prepare-safari.sh

--- prepare-safari.sh
+++ prepare-safari.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+defaults write com.apple.Safari CustomUserAgent 'UITests'
 echo "127.0.0.1 www.google-analytics.com" >> /etc/hosts
 echo "127.0.0.1 gtm.eduki.com" >> /etc/hosts
 echo "127.0.0.1 sdk.fra-01.braze.eu" >> /etc/hosts
