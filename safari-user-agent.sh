0afd31653e555e5b7ef3df189fb6b2e4e28a4a97
igo0r <igor.lantushenko@gmail.com>
2024-12-12 16:03:19 +0100

    Update safari-user-agent.sh

--- safari-user-agent.sh
+++ safari-user-agent.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-defaults write com.apple.Safari CustomUserAgent 'UITests'
+defaults write com.apple.Safari CustomUserAgent 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0) AppleWebKit/537.36 (KHTML, like Gecko) Version/15.0 Safari/537.36 UITests'
